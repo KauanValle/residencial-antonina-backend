13d0a5395e041618fa7be64a17d3f2fd6053121e
Kauan Valle <kauanvalle3@gmail.com>
2026-04-08 19:55:02 -0300

    alterando index

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://residencial-antonina-backend-production.up.railway.app'],
   credentials: true,
 }));
 
